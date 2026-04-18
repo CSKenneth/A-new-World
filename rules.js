8e39bf4374bea67cac51b8a638572d25f5c379fe
CSKenneth <ktran111@ucsc.edu>
2026-04-17 20:12:10 -0700

    A new world finished
    updated with new story and mechanics

---
+++ rules.js
@@ -0,0 +1,73 @@
+class Start extends Scene {
+    create() {
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
+        
+       
+        
+
+        this.engine.addChoice("Begin the story");
+
+        
+    }
+
+    handleChoice() {
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
+    }
+}
+
+class Location extends Scene {
+    create(key) {
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        console.log("Location.create->locationData", locationData);
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
+
+        if(!this.engine.visited){
+            this.engine.visited = new Set();
+
+        }
+        this.engine.visited.add(key); 
+
+        if(locationData.Choices && locationData.Choices.length > 0) { // TODO: check if the location has any Choices
+            console.log("yes");
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                console.log("choice", choice)
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
+                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+            }
+
+        } else {
+            this.engine.addChoice("The End.")
+        }
+        
+        if(locationData.altChoice){
+            for(let alt of locationData.altChoice){
+                if(!alt.requires || this.engine.visited.has(alt.requires)){
+                    this.engine.addChoice(alt.Text, alt)
+
+                }
+
+            }
+
+        } 
+    }
+
+    handleChoice(choice) {
+  if (choice) {
+
+    this.engine.show("&gt; " + choice.Text);
+    this.engine.gotoScene(Location, choice.Target);
+
+  } else {
+    this.engine.gotoScene(End);
+  }
+    }
+}
+
+class End extends Scene {
+    create() {
+        this.engine.show("<hr>");
+        this.engine.show(this.engine.storyData.Credits);
+    }
+}
+
+Engine.load(Start, 'myStory.json');
